c47691910b2af101bb7207428f7dafd4218b30f8
Boryhit <mateusvog@gmail.com>
2025-08-28 18:54:09 -0300

    teste-error

--- src/controllers/example.controllers.ts
+++ src/controllers/example.controllers.ts
@@ -4,6 +4,10 @@ class ExampleController {
     async helloWorld(req: Request, res: Response) {
         return res.send('Hello Typescript');
     }
+
+    testError(req: Request,res:Response){
+        throw new Error('Teste de erro');
+    }
 };
 
 export { ExampleController };
