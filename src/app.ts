7315dc40c16d91e17da6c8cc004efb57cfc39ec4
Boryhit <mateusvog@gmail.com>
2025-10-08 18:57:11 -0300

    Adicionando CID nos requests

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Express } from 'express';
 import { routes } from './routes/routes';
 import cors from 'cors';
+
+import { cid } from './middlewares/cid.middleware';
 import { log } from './middlewares/log.middleware';
 import { errorHandling } from './middlewares/error_handling.middleware';
 import { database } from './database/database';
 
 const app: Express = express();
 
+app.use(cid); //Middleware cid
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +20,9 @@ app.use(log);
 app.use(routes);
 
 // Testar a conexão
-(async () => {
-    console.log(await database.query('select now() as data_atual', []));
-})();
+// (async () => {
+//     console.log(await database.query('select now() as data_atual', []));
+// })();
 
 //Tratativa de erros da aplicação
 app.use(errorHandling);
